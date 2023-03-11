f65ed486257b241a5721a8fe7dfae559bcd5b561
paras <paras@gmail.com>
2023-03-11 14:23:55 +0530

    Integrated update project api after login while creating a project

--- src/Components/AboutUs.js
+++ src/Components/AboutUs.js
@@ -1,16 +1,9 @@
-import React, {useEffect} from 'react';
+import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import Navbar from './Navbar';
+import Navbar from "./Navbar";
 
 function AboutUs() {
   const navigate = useNavigate();
-
-  useEffect(() => {
-    document.getElementById('about-link').classList.add('active');
-    document.getElementById('price-link').classList.remove('active');
-    document.getElementById('contact-link').classList.remove('active');
-    document.getElementById('dashboard-link').classList.remove('active');
-  }, []);
 
   return (
     <div>
@@ -18,18 +11,18 @@ function AboutUs() {
       <div className="main-parant-14">
         <section className="sec-1">
           <div>
-            <p className="top-home pt-3 text-left">{'Home > About Us'}</p>
+            <p className="top-home pt-3 text-left">{"Home > About Us"}</p>
           </div>
           <div className="container brdr-bottom">
             <div className="row">
               <div className="col-lg-12">
                 <div className="cntr-area">
                   <div className="modeler-box">
-
                     <div className="grid-wlcm">
-                      <h1 className="WELCOME-title">PAGE UNDER <span className="cimr">DEVELOPMENT</span></h1>
+                      <h1 className="WELCOME-title">
+                        PAGE UNDER <span className="cimr">DEVELOPMENT</span>
+                      </h1>
                     </div>
-
                   </div>
                 </div>
               </div>
--- src/Components/ContactUs.js
+++ src/Components/ContactUs.js
@@ -1,16 +1,9 @@
-import React, { useEffect } from 'react';
+import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import Navbar from './Navbar';
+import Navbar from "./Navbar";
 
 function ContactUs() {
   const navigate = useNavigate();
-
-  useEffect(() => {
-    document.getElementById('contact-link').classList.add('active');
-    document.getElementById('price-link').classList.remove('active');
-    document.getElementById('dashboard-link').classList.remove('active');
-    document.getElementById('about-link').classList.remove('active');
-  }, []);
 
   return (
     <div>
@@ -18,18 +11,18 @@ function ContactUs() {
       <div className="main-parant-14">
         <section className="sec-1">
           <div>
-            <p className="top-home pt-3 text-left">{'Home > Contact Us'}</p>
+            <p className="top-home pt-3 text-left">{"Home > Contact Us"}</p>
           </div>
           <div className="container brdr-bottom">
             <div className="row">
               <div className="col-lg-12">
                 <div className="cntr-area">
                   <div className="modeler-box">
-
                     <div className="grid-wlcm">
-                      <h1 className="WELCOME-title">PAGE UNDER <span className="cimr">DEVELOPMENT</span></h1>
+                      <h1 className="WELCOME-title">
+                        PAGE UNDER <span className="cimr">DEVELOPMENT</span>
+                      </h1>
                     </div>
-
                   </div>
                 </div>
               </div>
