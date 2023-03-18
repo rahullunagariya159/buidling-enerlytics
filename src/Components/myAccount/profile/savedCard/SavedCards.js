3146159c43140d44636a6de80ddc820a20d5adf2
paras <hadiya.yagnesh@gmail.com>
2023-03-18 19:26:27 +0530

    credit card UI in profile screen

--- src/Components/myAccount/profile/savedCard/SavedCards.js
+++ src/Components/myAccount/profile/savedCard/SavedCards.js
@@ -1,26 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import { HeaderTitle, HorizontalLine, Wrapper } from "../personalDetails/style";
 import {
   ActionWrp,
   AddCard,
   CardDetails,
   CardDetailsContent,
+  CardNumber,
   CardSection,
   CardTitle,
+  CardWrp,
+  CheckBoxWrp,
   EditButton,
   EmptyCard,
+  EmptyCardWrp,
   SmallText,
+  SmallTextGray,
+  SmallTextTopaz,
   SmallVerticalLine,
 } from "./style";
 
 const SavedCards = () => {
+  const [newCard, setNewCard] = useState(false);
+  const [emptyCard, setEmptyCard] = useState(false);
   return (
     <Wrapper>
       <HeaderTitle>SAVED CARDS</HeaderTitle>
       <HorizontalLine />
       <CardSection>
-        <EmptyCard>No saved card</EmptyCard>
-        <AddCard>
+        {newCard && <EmptyCard>No saved card</EmptyCard>}
+        {!newCard && (
+          <CardWrp>
+            <img src="assets/img/profile/visaCard.png" alt="" />
+            <CheckBoxWrp>
+              <input type="checkbox" id="check1" />
+            </CheckBoxWrp>
+          </CardWrp>
+        )}
+        <AddCard onClick={() => setNewCard(!newCard)}>
           <img src="assets/img/profile/plus.png" alt="" />
           <SmallText>Add new</SmallText>
         </AddCard>
@@ -33,9 +49,73 @@ const SavedCards = () => {
           <EditButton>Edit</EditButton>
         </ActionWrp>
       </CardDetails>
-      <CardDetailsContent>
-        <EmptyCard>No card details</EmptyCard>
-      </CardDetailsContent>
+      {emptyCard && (
+        <EmptyCardWrp>
+          <EmptyCard>No card details</EmptyCard>
+        </EmptyCardWrp>
+      )}
+      {!emptyCard && (
+        <CardDetailsContent>
+          <CardDetails>
+            <ActionWrp>
+              <SmallText>Harry James Potter</SmallText>
+              <SmallVerticalLine />
+              <SmallTextGray>Credit card</SmallTextGray>
+            </ActionWrp>
+            <SmallTextTopaz>Default</SmallTextTopaz>
+          </CardDetails>
+          <CardNumber>
+            <SmallTextGray>****</SmallTextGray>
+            <SmallTextGray>-</SmallTextGray>
+            <SmallTextGray>****</SmallTextGray>
+            <SmallTextGray>-</SmallTextGray>
+            <SmallTextGray>****</SmallTextGray>
+            <SmallTextGray>-</SmallTextGray>
+            <SmallTextGray>0000</SmallTextGray>
+          </CardNumber>
+          <CardDetails>
+            <ActionWrp>
+              <SmallText>Expiry date:</SmallText>
+              <SmallTextGray>01/25</SmallTextGray>
+              <SmallVerticalLine />
+              <SmallText>CVV:</SmallText>
+              <SmallTextGray>***</SmallTextGray>
+            </ActionWrp>
+            <img src="assets/img/profile/visa-credit-card.png" alt="" />
+          </CardDetails>
+        </CardDetailsContent>
+      )}
+      {!emptyCard && (
+        <CardDetailsContent>
+          <CardDetails>
+            <ActionWrp>
+              <SmallText>Harry James Potter</SmallText>
+              <SmallVerticalLine />
+              <SmallTextGray>Credit card</SmallTextGray>
+            </ActionWrp>
+            <SmallTextTopaz>Default</SmallTextTopaz>
+          </CardDetails>
+          <CardNumber>
+            <SmallTextGray>****</SmallTextGray>
+            <SmallTextGray>-</SmallTextGray>
+            <SmallTextGray>****</SmallTextGray>
+            <SmallTextGray>-</SmallTextGray>
+            <SmallTextGray>****</SmallTextGray>
+            <SmallTextGray>-</SmallTextGray>
+            <SmallTextGray>0000</SmallTextGray>
+          </CardNumber>
+          <CardDetails>
+            <ActionWrp>
+              <SmallText>Expiry date:</SmallText>
+              <SmallTextGray>01/25</SmallTextGray>
+              <SmallVerticalLine />
+              <SmallText>CVV:</SmallText>
+              <SmallTextGray>***</SmallTextGray>
+            </ActionWrp>
+            <img src="assets/img/profile/visa-credit-card.png" alt="" />
+          </CardDetails>
+        </CardDetailsContent>
+      )}
     </Wrapper>
   );
 };
--- src/Components/myAccount/profile/savedCard/style.js
+++ src/Components/myAccount/profile/savedCard/style.js
@@ -2,8 +2,10 @@ import styled from "styled-components";
 export const CardSection = styled.div`
   display: flex;
   margin: 30px 0 40px 0;
-  gap: 100px;
+  gap: 28px;
   align-items: center;
+  max-width: 508px;
+  word-wrap: break-word;
 `;
 export const EmptyCard = styled.div`
   font: normal normal medium 16px/25px Poppins;
@@ -20,6 +22,7 @@ export const AddCard = styled.div`
   align-items: center;
   cursor: pointer;
   gap: 7px;
+  margin-left: 50px;
   img {
     width: 23px;
     height: 23px;
@@ -51,12 +54,57 @@ export const EditButton = styled.div`
   letter-spacing: 0px;
   color: #b9b9b9;
 `;
-export const CardDetailsContent = styled.div`
-  height: 111px;
+export const EmptyCardWrp = styled.div`
+  min-height: 111px;
   background: #f9f9f9 0% 0% no-repeat padding-box;
   border-radius: 4px;
   margin-top: 5px;
   padding-left: 30px;
   display: flex;
   align-items: center;
+`;
+export const CardWrp = styled.div`
+  position: relative;
+  img {
+    width: 109px;
+    height: 70px;
+  }
+`;
+export const CheckBoxWrp = styled.div`
+  width: 14px;
+  height: 14px;
+  /* background: #ffffff 0% 0% no-repeat padding-box;
+  border: 2px solid #8d8d8d;
+  border-radius: 4px; */
+  position: absolute;
+  right: -6px;
+  top: -10px;
+`;
+
+export const CardDetailsContent = styled.div`
+  padding: 15px;
+  background: #f9f9f9 0% 0% no-repeat padding-box;
+  border-radius: 4px;
+  margin-top: 5px;
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  margin-bottom: 40px;
+`;
+export const SmallTextGray = styled.span`
+  letter-spacing: 0px;
+  color: #b9b9b9;
+  font-size: 16px;
+  font-weight: 500;
+  font-family: poppins;
+`;
+export const SmallTextTopaz = styled.div`
+  font: normal normal medium 10px/16px Poppins;
+  letter-spacing: 0px;
+  color: #00bec1;
+`;
+export const CardNumber = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 2px;
 `;
